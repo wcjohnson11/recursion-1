4d7078783a4c6c6f8298131a8af142eec0295dc3
Gary Johnson <jazzbassjohnson@gmail.com>
2014-05-05 12:23:27 -0700

    getElementsByClassName editted but not complete yet

--- src/getElementsByClassName.js
+++ src/getElementsByClassName.js
@@ -4,8 +4,33 @@
 // };
 
 // But instead we're going to implement it from scratch:
-var getElementsByClassName = function(className){
-	
+var getElementsByClassName = function(className, node){
+	var element = node || $('body');
+	var collection = [], n;
+
+	if(element.children.length === 0 && element.className === className){
+			return element;
+	}
+
+	if(element.children.length > 0){
+		for(var i = 0; i<element.children.length-1; i++){
+			if (element.className === className) {
+				collection.push(element);
+			}else{
+				n = getElementsByClassName(className, element[i]);
+				collection.push(n);
+			}
+			collection.push($(n));
+			console.log(collection);
+		}		
+	}
+		return collection;
   // your code here
 };
 
+// var div = $("div");
+// for (var i = 0; i < div.length; i++) {
+//     if (div[i].className == 'container') {
+//         div[i].style.backgroundColor = '#000';
+//     }
+// }
