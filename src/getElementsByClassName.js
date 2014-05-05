cdeb283cdb8dd582c47a1a883181b5c167590d3c
Gary Johnson <jazzbassjohnson@gmail.com>
2014-05-05 09:37:03 -0700

    Complete

--- src/getElementsByClassName.js
+++ src/getElementsByClassName.js
@@ -5,5 +5,7 @@
 
 // But instead we're going to implement it from scratch:
 var getElementsByClassName = function(className){
+	
   // your code here
 };
+
--- src/stringifyJSON.js
+++ src/stringifyJSON.js
@@ -3,7 +3,29 @@
 
 // but you don't so you're going to write it from scratch:
 var stringifyJSON = function(obj) {
-  // your code goes here
-  
-};
+	var objType = typeof obj;
+	if(objType !== "object" || obj === null){
+		if(objType === "string"){
+			obj = '"'+obj+'"';
+		}
+		return String(obj);
+	}else{
+		var key, value, storage = [], arrTest = (obj && obj.constructor === Array);
 
+		for(key in obj){
+			value = obj[key];
+			objType = typeof value;
+			if(obj.hasOwnProperty(key)){
+				if(objType === "function" || objType === undefined){
+					return "{}";
+				}else if(objType === "string"){
+					value = '"' + value + '"';
+				}else if(objType === "object" && value !== null){
+				value = stringifyJSON(value);
+				}
+				storage.push((arrTest ? "" : '"' + key + '":') + String(value));
+			}
+		}
+		return (arrTest ? "[" : "{") + String(storage) + (arrTest ? "]" : "}");
+	}
+};
