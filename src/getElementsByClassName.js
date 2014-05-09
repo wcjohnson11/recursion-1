bf93f98f9edf379a56c520c2d404efbf081be8c7
Gary Johnson <jazzbassjohnson@gmail.com>
2014-05-09 11:11:27 -0700

    Completed getElementsByClassName

--- src/getElementsByClassName.js
+++ src/getElementsByClassName.js
@@ -4,33 +4,20 @@
 // };
 
 // But instead we're going to implement it from scratch:
-var getElementsByClassName = function(className, node){
-	var element = node || $('body');
-	var collection = [], n;
-
-	if(element.children.length === 0 && element.className === className){
-			return element;
+var getElementsByClassName = function(className){
+	var collection = [], index = 0, pattern = new RegExp("(^|\\s)"+className+"(\\s|$)");
+	
+	function classFilter(element) {
+		if ( pattern.test(element.className) ){
+			collection[index] = element;
+			index++;
+		} 
+		var children = element.children;
+		for(var i = 0; i < children.length; i++) {
+		  classFilter(children[i]);
+		}
 	}
-
-	if(element.children.length > 0){
-		for(var i = 0; i<element.children.length-1; i++){
-			if (element.className === className) {
-				collection.push(element);
-			}else{
-				n = getElementsByClassName(className, element[i]);
-				collection.push(n);
-			}
-			collection.push($(n));
-			console.log(collection);
-		}		
-	}
-		return collection;
-  // your code here
+	
+	classFilter(document);
+	return collection;
 };
-
-// var div = $("div");
-// for (var i = 0; i < div.length; i++) {
-//     if (div[i].className == 'container') {
-//         div[i].style.backgroundColor = '#000';
-//     }
-// }
